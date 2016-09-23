9040fd0c7e5eb8b6d636abe41168eec87a9a43a9
Kieran Whyte <G00254624@gmit.ie>
2016-09-23 22:43:47 +0100

    UI Commit #2

--- ToDoAppFrontEnd/WebContent/Controller.js
+++ ToDoAppFrontEnd/WebContent/Controller.js
@@ -1,7 +1,9 @@
 var app = angular.module('MainApp', ['ngRoute']);
 
+//Provides page routing for the application
 app.config(function($routeProvider){
 	
+	//Root goes to login page, any unknown URLs just redirect to the login page.
 	$routeProvider
 	.when('/', { 
 		templateUrl: 'Login.html'
@@ -9,5 +11,14 @@ app.config(function($routeProvider){
 	.otherwise({
 		redirectTo: '/'
 	});
+	
+});
+
+
+
+app.controller('loginController', function($scope) {
+	
+	//controller for login.html, has scope for that page
+	//do stuff in here
 	
 });
