8ea4590fd2dc538dd6562b3d9610c11d43c78392
Kieran Whyte <G00254624@gmit.ie>
2016-10-06 12:38:43 +0100

    User Profile Page and API Bind

---
+++ WebContent/JavascriptClasses/UserUpdatePassword.js
@@ -0,0 +1,10 @@
+"use strict";
+//Class defining the dot that pacman character tries to catch
+class UserUpdatePassword
+{
+    //Constructor for co-ordinates, radius, coulour and context.
+    constructor(password)
+    {
+        this.password = password;
+    }
+}
